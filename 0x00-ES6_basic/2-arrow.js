eccd1d15b79de471543e49724008d83a14dd6bf7
Rania FATHALLAH <rania14fathallah@gmail.com>
2024-10-13 00:59:18 +0100

    0

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,8 +2,9 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
+
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
-  }
+  };
 }
