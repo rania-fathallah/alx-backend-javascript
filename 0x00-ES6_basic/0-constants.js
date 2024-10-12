127d9424475d9d9a9f4cf34078be99356e2e81ab
Rania FATHALLAH <rania14fathallah@gmail.com>
2024-10-13 00:48:02 +0100

    0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,16 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
+  const task = 'I prefer const when I can.';
 
-    return task;
+  return task;
 }
   
 export function getLast() {
-    return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
   
-    return combination;
+  return combination;
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,7 +2,7 @@ export default function iterateThroughObject(reportWithIterator) {
   const employees = [];
 
   for (const employee of reportWithIterator) {
-      employees.push(employee);
+    employees.push(employee);
   }
 
   return employees.join(' | ');
